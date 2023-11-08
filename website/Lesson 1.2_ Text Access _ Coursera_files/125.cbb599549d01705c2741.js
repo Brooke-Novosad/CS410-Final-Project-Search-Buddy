6fad672524e51baa2c6c711db6cebf8eddf77b56
pokeycat9 <novosad3@illinois.edu>
2023-11-08 15:15:51 -0600

    Revert "Added starting documents and transcripts"
    This reverts commit cc23f68b4b8b5b071d0f32424419bb1197da6080.

--- website/Lesson 1.2_ Text Access _ Coursera_files/125.cbb599549d01705c2741.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[125],{JISx:function(module,exports){Date.now()}}]);
-//# sourceMappingURL=125.cbb599549d01705c2741.js.map
--- website/Lesson 1.2_ Text Access _ Coursera_files/allStyles.80651bd772b439ce1707.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["allStyles"],[]]);
-//# sourceMappingURL=allStyles.80651bd772b439ce1707.js.map
