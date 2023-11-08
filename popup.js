7e43d68e04eaaada6e7a9f731d8a95670d69f113
pokeycat9 <novosad3@illinois.edu>
2023-11-08 16:37:03 -0600

    Added search bar

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-console.log('This is a popup!');
+console.log("hi");
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+
+function submitted(event) {
+  event.preventDefault();
+  const query = q.value;
+  console.log(query);
+}
+
+f.addEventListener('submit', submitted);
