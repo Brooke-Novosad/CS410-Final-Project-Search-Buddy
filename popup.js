63e0b7b76afae367ab47c1dbf1d51f8bdcced285
hehe <hehe@wirelessprv-10-195-61-251.near.illinois.edu>
2023-11-29 14:31:41 -0600

    searching animals lol

--- popup.js
+++ popup.js
@@ -6,6 +6,22 @@ function submitted(event) {
   event.preventDefault();
   const query = q.value;
   console.log(query);
+  search_animal();
 }
 
 f.addEventListener('submit', submitted);
+
+function search_animal() { 
+  let input = document.getElementById('query').value 
+  input=input.toLowerCase(); 
+  let x = document.getElementsByClassName('animals'); 
+    
+  for (i = 0; i < x.length; i++) {  
+      if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+          x[i].style.display="none"; 
+      } 
+      else { 
+          x[i].style.display="list-item";                  
+      } 
+  } 
+} 
