0aeb810d34ce1bf1fd7bac9a8442c28415d882df
rainyyyan <rainyyyan@gmail.com>
2023-11-30 16:02:03 -0600

    Added ranking function to flask app

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # import the Flask library
 from flask import Flask, render_template, request
+import ranking_function
 
 
 # Create the Flask instance and pass the Flask constructor the path of the correct module
 app = Flask(__name__)
 
 
+
 @app.route('/', methods=['GET', 'POST'])
-def squarenumber():
+def runApp():
+	file_name_map, documents, bm25 = ranking_function.startup()
 # If method is POST, get the query entered by user
 # Use the ranker to get the top 10 documents and render the document names on the webpage
 	if request.method == 'POST':
@@ -15,10 +18,19 @@ def squarenumber():
 			return "<html><body> <h1>Invalid number</h1></body></html>"
 		else:
 			# placeholder just multiplies the number for now
-			number = request.form['query']
-			sq = int(number) * int(number)
+			query = request.form['query']
+			results = ranking_function.search(query, file_name_map, bm25)
 			return render_template('answer.html', 
-							squareofnum=sq)
+							result1=results[0],
+							result2=results[1],
+							result3=results[2],
+							result4=results[3],
+							result5=results[4],
+							result6=results[5],
+							result7=results[6],
+							result8=results[7],
+							result9=results[8],
+							result10=results[9])
 	# If the method is GET,render the HTML page to the user
 	if request.method == 'GET':
 		return render_template("home.html")
--- ranking_function.py
+++ ranking_function.py
@@ -8,58 +8,49 @@ import io
 import numpy as np
 from rank_bm25 import BM25Okapi
 
-if __name__ == "__main__":
+def startup():
 
     file_name_map = {}
     documents = [] 
 
-    def startup():
-        file_idx = 0
-    
-        slides = "lecture_slides_extractions"
-        transcripts = "lecture_transcripts"
+    file_idx = 0
 
-        for filename in os.listdir(transcripts):
-            fp = os.path.join(transcripts, filename)
-            in_f = open(fp)
+    slides = "lecture_slides_extractions"
+    transcripts = "lecture_transcripts"
+
+    for filename in os.listdir(transcripts):
+        fp = os.path.join(transcripts, filename)
+        in_f = open(fp)
+        file_name_map[file_idx] = filename
+        doc = ""
+        for num, line in enumerate(in_f):
+            doc = doc + line.lower().strip() + " "
+        documents.append(doc)
+        file_idx += 1
+
+    for filename in os.listdir(slides):
+            fp = os.path.join(slides, filename)
+            in_f = io.open(fp, encoding='UTF-16')
             file_name_map[file_idx] = filename
             doc = ""
             for num, line in enumerate(in_f):
                 doc = doc + line.lower().strip() + " "
             documents.append(doc)
-            file_idx += 1
+            file_idx += 1    
 
-        for filename in os.listdir(slides):
-                fp = os.path.join(slides, filename)
-                in_f = io.open(fp, encoding='UTF-16')
-                file_name_map[file_idx] = filename
-                doc = ""
-                for num, line in enumerate(in_f):
-                    doc = doc + line.lower().strip() + " "
-                documents.append(doc)
-                file_idx += 1     
+    tokenized_corpus = [doc.split(" ") for doc in documents]
 
-    def searchFun():
-        tokenized_corpus = [doc.split(" ") for doc in documents]
+    bm25 = BM25Okapi(tokenized_corpus)
 
-        bm25 = BM25Okapi(tokenized_corpus)
+    return file_name_map, documents, bm25
 
-        def search(query, top_k):
+def search(query,file_name_map, bm25):
+    top_k = 10
 
-            tokenized_query = query.split(" ")
+    tokenized_query = query.split(" ")
 
-            doc_scores = bm25.get_scores(tokenized_query)
+    doc_scores = bm25.get_scores(tokenized_query)
 
-            return np.argsort(doc_scores)[::-1][:top_k]
+    results = np.argsort(doc_scores)[::-1][:top_k]
 
-        idx = search("inverse document frequency", 10)
-
-        out_f = open("results.txt", 'w')
-        for i in idx:
-            out_f.write(file_name_map[i] + '\n')
-
-    args = sys.argv[1:]
-    if args[0] == "start":
-        startup()
-    else: 
-        searchFun()
+    return [file_name_map[i] for i in results]
