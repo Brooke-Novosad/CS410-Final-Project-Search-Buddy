54cd03b6947258fcbada6de6a21fb484c1686b73
Drshika A <67125579+drshika@users.noreply.github.com>
2023-11-30 16:28:46 -0600

    adding descriptions to search results

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
 # import the Flask library
 from flask import Flask, render_template, request
+import os
+import io
 import ranking_function
 
 
 # Create the Flask instance and pass the Flask constructor the path of the correct module
 app = Flask(__name__)
 
+def generateBlurbs(results):
+	slides = "lecture_slides_extractions"
+	transcripts = "lecture_transcripts"
+	blurblist = []
+	for r in results:
+		if len(r) > 15:
+			#transcript
+			fp = os.path.join(transcripts, r)
+			in_f = open(fp)
+			countline = 0
+			curchar = 0
+			blurb = ""
+			for line in in_f:	
+				if countline < 20:
+					countline += 1
+				else:
+					for c in line:
+						if curchar < 150:
+							curchar+=1
+							blurb+= c
+			blurblist.append(blurb)
+					
+					
+		else:
+			#slide
+			fp = os.path.join(slides, r)
+			in_f = io.open(fp, encoding='UTF-16')
+			countline = 0
+			curchar = 0
+			blurb = ""
+			for line in in_f:	
+				if countline < 20:
+					countline += 1
+				else:
+					for c in line:
+						if curchar < 150:
+							curchar+=1
+							blurb+= c
+			blurblist.append(blurb)
+	return blurblist 
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,17 +62,38 @@ def runApp():
 			# placeholder just multiplies the number for now
 			query = request.form['query']
 			results = ranking_function.search(query, file_name_map, bm25)
+			descriptions = generateBlurbs(results)
+			# descriptions  = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", 
+			# 		"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." ]
 			return render_template('answer.html', 
 							result1=results[0],
+							description1 =descriptions[0],
 							result2=results[1],
+							description2 = descriptions[1],
 							result3=results[2],
+							description3 = descriptions[2],
 							result4=results[3],
+							description4 = descriptions[3],
 							result5=results[4],
+							description5 = descriptions[4],
 							result6=results[5],
+							description6 = descriptions[5],
 							result7=results[6],
+							description7 = descriptions[6],
 							result8=results[7],
+							description8 = descriptions[7],
 							result9=results[8],
-							result10=results[9])
+							description9 = descriptions[8],
+							result10=results[9],
+							description10 = descriptions[9])
 	# If the method is GET,render the HTML page to the user
 	if request.method == 'GET':
 		return render_template("home.html")
